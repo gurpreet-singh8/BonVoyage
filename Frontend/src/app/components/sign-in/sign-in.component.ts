30c9cd873618a53d1a7771c24be8b2f61052569d
lakavath.sonia <lakavath.sonia@globallogic.com>
2024-06-11 00:44:30 +0530

    signin ts files

--- Frontend/src/app/components/sign-in/sign-in.component.ts
+++ Frontend/src/app/components/sign-in/sign-in.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ServiceService } from '../../service.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sign-in',
@@ -18,16 +19,20 @@ export class SignInComponent {
     
   };
 
-  constructor(private userService: ServiceService, private router: Router) {}
-  onSubmit() {
+  constructor(private authService: ServiceService, private router: Router) {}
 
-    this.userService.register(this.user).subscribe((data)=>{
-      console.log('User Logined successfully', data);
-       this.router.navigate(['/']);
-    }, error => {
-      console.error('Error registering user', error);
-    });
+  onSubmit() {
+    this.authService.login(this.user.email, this.user.password).subscribe(
+      data => {
+        console.log('User logged in successfully', data);
+        this.router.navigate(['/']);
+      },
+      error => {
+        console.error('Error logging in user', error);
+      }
+    );
+  }
    
   }
 
-}
+
